bb6ce9147a81604dc7097fe033701aef1ef9448c
mishakolyvanov <mishakolyvanov@gmail.com>
2021-10-02 21:31:58 +0500

    Added validation

--- api/serializers.py
+++ api/serializers.py
@@ -7,11 +7,16 @@ class EventSerializer(serializers.ModelSerializer):
     cpm = serializers.SerializerMethodField('cpm_count')
 
     def cpc_count(self, event):
+        if event.clicks == 0:
+            return 'На событие ни разу не кликнули'
         return event.cost / event.clicks
 
     def cpm_count(self, event):
-        cpm = event.cost / event.views * 1000
-        return float('{:.2f}'.format(cpm))
+        if event.views == 0:
+            return 'Событие никому не показали'
+        else:
+            cpm = event.cost / event.views * 1000
+            return float('{:.2f}'.format(cpm))
 
     class Meta:
         model = Event
--- event/models.py
+++ event/models.py
@@ -1,11 +1,13 @@
 import datetime
+from decimal import Decimal
 
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Event(models.Model):
     date = models.DateField(default=datetime.datetime.now)
-    views = models.IntegerField(default=0)
-    clicks = models.IntegerField(default=0)
-    cost = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    views = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+    clicks = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+    cost = models.DecimalField(max_digits=20, decimal_places=2, default=0, validators=[MinValueValidator(Decimal('0.0'))])
 
