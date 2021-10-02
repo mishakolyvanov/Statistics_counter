512862c12535dd92b03694481544dcc95c96c249
mishakolyvanov <mishakolyvanov@gmail.com>
2021-10-02 16:07:42 +0500

    Added model Event

--- event/models.py
+++ event/models.py
@@ -1,3 +1,11 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    date = models.DateField(default=datetime.datetime.now)
+    views = models.IntegerField(default=0)
+    clicks = models.IntegerField(default=0)
+    cost = models.DecimalField(max_digits=20, decimal_places=2)
+
