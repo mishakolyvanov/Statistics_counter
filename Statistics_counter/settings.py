a0ac8351b42d2f3b21ed67a85259bf7745c3b83d
mishakolyvanov <mishakolyvanov@gmail.com>
2021-10-04 17:03:00 +0500

    Added sorting and date from to range filter

--- Statistics_counter/settings.py
+++ Statistics_counter/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'event',
     'rest_framework',
-    'api'
+    'api',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
--- api/views.py
+++ api/views.py
@@ -1,13 +1,26 @@
+from django_filters import DateFromToRangeFilter
+from django_filters.rest_framework import FilterSet, DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.filters import OrderingFilter
 
 from event.models import Event
 from . import serializers
 
 
+class EventFilter(FilterSet):
+    date = DateFromToRangeFilter(field_name='date')
+
+    class Meta:
+        model = Event
+        fields = ['date']
+
+
 class EventList(generics.ListCreateAPIView):
     queryset = Event.objects.all()
     serializer_class = serializers.EventSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_class = EventFilter
 
     def perform_create(self, serializer):
         serializer.save()
